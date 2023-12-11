9e46e3701569d2924559d044dd86057a00fe0a0c
syedalihasany <49318734+syedalihasany@users.noreply.github.com>
2023-12-10 23:54:53 -0600

    added a README.TXT

--- train.py
+++ train.py
@@ -1,46 +1,62 @@
 import numpy as np
 from matplotlib import pyplot as plt
-
-from tensorflow.keras.models import Sequential
-
+import tensorflow as tf
 from tensorflow.keras.layers import *
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.preprocessing import *
-
-import tensorflow as tf
-from tensorflow.keras.layers import InputLayer, Conv2D, MaxPooling2D, Flatten, Dense
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-import matplotlib.pyplot as plt
-
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
 
+# ########################### MODEL TRAINING SCRIPT ########################################
+# This should work with any python environment with python 3 and and tensorflow > 2.0.0
+# but bear in mind that I used the following environment to run this script
+# Requirements:
+# tensorflow 2.13.1
+# python 3.8.10
+# OS: Ubuntu 20.04.6 LTS
+# numpy 1.24.3
+# matplotlib 3.7.4
+#############################################################################################
+
+
+
+# repertoire1 is the path to our card data set
 repertoire1 = 'train/'
-repertoire2 = 'validation/'
 
+# Defining the image size
 image_length = 224
 image_width = 224
 batch_size = 32
 image_size = (image_length, image_width)
 
+seed_value = 42
 
-train = image_dataset_from_directory(
+train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     repertoire1,
+    labels='inferred',
     label_mode='categorical',
-    image_size=image_size
+    image_size=image_size,
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='training',
+    seed=seed_value
 )
 
-test = image_dataset_from_directory(
-    repertoire2,
+validation_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    repertoire1,
+    labels='inferred',
     label_mode='categorical',
-    image_size=image_size
+    image_size=image_size,
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='validation',
+    seed=seed_value 
 )
 
-data_augmentation = Sequential ([
-    RandomFlip("horizontal", input_shape = (image_length, image_width, 3)),
-    RandomRotation (0.3),
-    RandomZoom (0.2)
+data_augmentation = Sequential([
+    RandomFlip("horizontal", input_shape=(image_length, image_width, 3)),
+    RandomRotation(0.3),
+    RandomZoom(0.2)
 ])
 
 model = Sequential()
@@ -64,19 +80,18 @@ model.add(Flatten())
 model.add(Dense(256, activation='relu'))
 model.add(Dense(53, activation='softmax'))
 
-#model = tf.keras.models.load_model("best_model.h5", compile=False)
-
 model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=1e-3), metrics=['accuracy'])
 
-# Define Early Stopping callback
+# Defining Early Stopping callback to store best weights
 early_stopping = EarlyStopping(monitor='val_accuracy', patience=100, restore_best_weights=True)
 
-# Define ModelCheckpoint callback to save the best model
+# Defining ModelCheckpoint callback to save the best model
 model_checkpoint = ModelCheckpoint("best_model_4.h5", save_best_only=True)
 
+
 history = model.fit(
-    train,
-    validation_data=test,
+    train_dataset,
+    validation_data=validation_dataset,
     epochs=100,
     verbose=1,
     callbacks=[early_stopping, model_checkpoint]
@@ -90,11 +105,11 @@ val_loss = history.history['val_loss']
 
 epochs_range = range(len(acc))
 
-# Create a figure with two subplots
-plt.figure(figsize=(12, 6))
+# printing the training and validation accuracy AND training and validation loss graphs
 
-# Plot training and validation accuracy
-plt.subplot(1, 2, 1)  # 1 row, 2 columns, first subplot
+plt.figure(figsize=(12, 12))
+
+plt.subplot(2, 1, 1)
 plt.plot(epochs_range, acc, label='Training Accuracy', color='blue')
 plt.plot(epochs_range, val_acc, label='Validation Accuracy', color='red')
 plt.title('Training and Validation Accuracy')
@@ -102,8 +117,8 @@ plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.legend()
 
-# Plot training and validation loss
-plt.subplot(1, 2, 2)  # 1 row, 2 columns, second subplot
+
+plt.subplot(2, 1, 2)
 plt.plot(epochs_range, loss, label='Training Loss', color='blue')
 plt.plot(epochs_range, val_loss, label='Validation Loss', color='red')
 plt.title('Training and Validation Loss')
@@ -111,7 +126,6 @@ plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.legend()
 
-# Save the figure
 plt.savefig("training_validation_curves_aug.png")
 
 plt.show()
